46d7bce3851e1f41a985a853ea3a521a4adcae96
HUSH-Wang <dzyxwanglizhou@gmail.com>
2020-02-01 21:48:01 +0800

    Add 3rd part OpenClash

---
+++ x86.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+# Add luci-app-OpenClash package
+git clone https://github.com/vernesong/OpenClash.git ../openclash
+cp ../openclash/luci-app-openclash ./package/feeds/packages/luci-app-openclash
