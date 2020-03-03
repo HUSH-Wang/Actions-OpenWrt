21c8d160f8457f19d0c3eac40b9f365db7ea0637
HUSH-Wang <dzyxwanglizhou@gmail.com>
2020-03-03 13:39:45 +0800

    Add serverchan to x86

--- 3rdpkgs.sh
+++ 3rdpkgs.sh
@@ -7,6 +7,9 @@
 # Blog: https://p3terx.com
 #=================================================
 
-# Add luci-app-OpenClash package
+# Add luci-app-openclash package
 git clone https://github.com/vernesong/OpenClash.git ../openclash
 cp -r ../openclash/luci-app-openclash ./package/feeds/packages/luci-app-openclash
+# Add luci-app-serverchan package
+git clone https://github.com/tty228/luci-app-serverchan.git ../luci-app-serverchan
+cp -r ../luci-app-serverchan ./package/feeds/packages/luci-app-serverchan
