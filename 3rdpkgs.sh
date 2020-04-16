f00bcc3156adc4442475de5d71dccb786b2ebaad
HUSH-Wang <dzyxwanglizhou@gmail.com>
2020-04-16 17:16:07 +0800

    Update to add $$R

--- 3rdpkgs.sh
+++ 3rdpkgs.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #=================================================
 # Description: DIY script
@@ -7,9 +6,20 @@
 # Blog: https://p3terx.com
 #=================================================
 
-# Add luci-app-openclash package
-git clone https://github.com/vernesong/OpenClash.git ../openclash
-cp -r ../openclash/luci-app-openclash ./package/feeds/packages/luci-app-openclash
 # Add luci-app-serverchan package
 git clone https://github.com/tty228/luci-app-serverchan.git ../luci-app-serverchan
 cp -r ../luci-app-serverchan ./package/feeds/packages/luci-app-serverchan
+
+# Add ssr-plus
+#git clone https://github.com/fw876/helloworld.git ../ssr-plus
+#cp -r ../ssr-plus/luci-app-ssr-plus ./package/feeds/packagesluci-app-ssr-plus
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+
+# Add luci-app-openclash package
+#git clone https://github.com/vernesong/OpenClash.git ../openclash
+#cp -r ../openclash/luci-app-openclash ./package/feeds/packages/luci-app-openclash
+echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
+
+# feeds update & install
+./scripts/feeds update -a
+./scripts/feeds install -a
